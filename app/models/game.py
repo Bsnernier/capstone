fdf5a2cd804bff8ceda0d0dc82b72fed20ea865d
bsnernier <nbern817@gmail.com>
2021-08-24 18:14:55 -0500

    add library and review models/seed files

--- app/models/game.py
+++ app/models/game.py
@@ -1,5 +1,7 @@
 from .db import db
-
+from sqlalchemy.orm import relationship
+from .review import Review
+from .library import Library
 
 class Game(db.Model):
     __tablename__ = 'games'
@@ -10,3 +12,6 @@ class Game(db.Model):
     cover_url = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     summary = db.Column(db.String(255), nullable=False)
+
+    reviews = relationship("Review", order_by=Review.id, back_populates="game", uselist=False)
+    libraries = relationship("Library", order_by=Library.id, back_populates="game", uselist=False)
---
+++ app/seeds/libraries.py
@@ -0,0 +1,24 @@
+from app.models import db, Library
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_libraries():
+    demo = Library(
+        userId=1,
+        gameId=1,
+        status='started'
+        )
+
+    db.session.add(demo)
+
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and RESET IDENTITY
+# resets the auto incrementing primary key, CASCADE deletes any
+# dependent entities
+def undo_libraries():
+    db.session.execute('TRUNCATE libraries RESTART IDENTITY CASCADE;')
+    db.session.commit()
