2c2ec94c1dd042e4a373103f98ef31524e4d172e
bsnernier <nbern817@gmail.com>
2022-02-10 16:48:24 -0600

    add date column to review model

--- app/models/review.py
+++ app/models/review.py
@@ -1,6 +1,7 @@
 from .db import db
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 class Review(db.Model):
     __tablename__ = 'reviews'
@@ -10,6 +11,7 @@ class Review(db.Model):
     gameId = db.Column(db.Integer, ForeignKey("games.id"), nullable=False)
     text = db.Column(db.String(1000), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
 
     game = relationship("Game", back_populates="reviews")
     user = relationship("User", back_populates="reviews")
@@ -21,5 +23,6 @@ class Review(db.Model):
             'gameId': self.gameId,
             'text': self.text,
             'rating': self.rating,
-            'username': self.user.username
+            'username': self.user.username,
+            'date': self.date
         }
