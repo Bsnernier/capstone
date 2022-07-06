1747373fc30e94ba2b8d626a612a23fdd4aed0d6
bsnernier <nbern817@gmail.com>
2022-07-06 09:03:30 -0500

    add bottom padding to game cards, add some links to footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,7 +1,29 @@
 import "./Footer.css";
 
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <div className="footer">
+            {" "}
+            <div className="footer_links">
+                <a
+                    href="https://github.com/Bsnernier/capstone"
+                    class="fab fa-github-square icon"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    {" "}
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/nathaniel-bernier-899110207"
+                    class="fab fa-linkedin icon"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    {" "}
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
