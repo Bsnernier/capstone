11c15f493f7e5ad42ca1677225656745b625163a
bsnernier <nbern817@gmail.com>
2022-08-18 15:06:02 -0500

    fix empty genres from displaying on library page

--- react-app/src/components/Library/index.js
+++ react-app/src/components/Library/index.js
@@ -7,6 +7,7 @@ import "./Library.css";
 
 function Library() {
     const [shelf, setShelf] = useState();
+    let gamesWithGenre = [];
 
     const genreArr = [
         "Adventure",
@@ -31,15 +32,22 @@ function Library() {
         })();
     }, [dispatch, user.id]);
 
-    const checkGenre = (genre, game) => {
-        if (game?.genres.includes(genre)) {
+    const checkGenre = (genre) => {
+        gamesWithGenre = [];
+        shelf?.forEach((game) => {
+            if (game?.genres.includes(genre)) {
+                gamesWithGenre.push(game);
+            }
+        });
+
+        console.log(gamesWithGenre);
+        if (gamesWithGenre.length === 0) {
+            // console.log(gamesWithGenre);
+            return " ";
+        } else {
             return (
-                <div className="games_card" key={game?.id}>
-                    <a className="games_link" href={`/games/${game?.gameId}`}>
-                        <img className="games_image" src={game?.cover} alt="uh oh" />
-                    </a>
-                    <div className="games_title">{game?.title}</div>
-                    <div className="games_title">Progress: {game?.status}</div>
+                <div id={genre} className="genre_title">
+                    {genre}
                 </div>
             );
         }
@@ -49,33 +57,22 @@ function Library() {
         <div id="1" className="genres_container">
             {genreArr?.map((genre) => (
                 <div id="2" className="lib_games_container" key={genre}>
-                    <div id={genre} className="genre_title">
-                        {genre}
-                    </div>
+                    {checkGenre(genre)}
                     <div className="card_container">
-                        {shelf?.map(
-                            (game) =>
-                                checkGenre(genre, game) && (
-                                    <div key={game?.id}>{checkGenre(genre, game)}</div>
-                                )
-                        )}
+                        {gamesWithGenre
+                            ? gamesWithGenre?.map((game) => (
+                                  <div className="games_card" key={game?.id}>
+                                      <a className="games_link" href={`/games/${game?.gameId}`}>
+                                          <img className="games_image" src={game?.cover} alt="uh oh" />
+                                      </a>
+                                      <div className="games_title">{game?.title}</div>
+                                      <div className="games_title">Progress: {game?.status}</div>
+                                  </div>
+                              ))
+                            : " "}
                     </div>
                 </div>
             ))}
-            {/* Old Game Container for Reference */}
-            {/* <div className="games_container">
-        {console.log(shelf)}
-        <div>Adventure</div>
-        {shelf?.map((game) => (
-          <div className="games_card" key={game?.id}>
-            <a className="games_link" href={`/games/${game?.gameId}`}>
-              <img className="games_image" src={game?.cover} alt="uh oh" />
-            </a>
-            <div className="games_title">{game?.title}</div>
-            <div className="games_title">Progress: {game?.status}</div>
-          </div>
-        ))}
-      </div> */}
         </div>
     );
 }
